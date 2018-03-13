419f19f7e01cf0287cbfa434224906b69003065f
Rui Cabanita <rui+github@ruicabanita.com>
2018-03-13 19:13:13 +0000

    roles working!!

--- worker-roles/Role.Control.js
+++ worker-roles/Role.Control.js
@@ -7,28 +7,49 @@
  * mod.thing == 'a thing'; // true
  */
 
+// Workers | Harversters
 var sources = Game.spawns['Spawn1'].room.find(FIND_SOURCES_ACTIVE);
 var sourcesCount = sources.length;
-console.log('sourcesCount = ' + sourcesCount);
+console.log('Sources = ' + sourcesCount);
 var energyMissingPercent = (1-(Game.spawns['Spawn1'].room.energyAvailable)/(Game.spawns['Spawn1'].room.energyCapacityAvailable))*100;
-console.log('energyMissing(%) = ' + energyMissingPercent);
+console.log('energyMissing(%) = ' + Math.round(energyMissingPercent));
 var impHar = sourcesCount * energyMissingPercent;
-console.log('impHar = ' + impHar);
+
+// Workers | Upgraders
 var impUpg = 50;
-var impTotal = impHar + impUpg;
+
+// Workers | Builders
+var cSites = Game.spawns['Spawn1'].room.find(FIND_CONSTRUCTION_SITES);
+var cSitesCount = cSites.length;
+var damagedStructures = Game.spawns['Spawn1'].room.find(FIND_MY_STRUCTURES);
+var damagedStructuresCount = damagedStructures.length;
+var missingStructureHitpoints = 0;
+var totalStructureHitpoints = 0;
+for(var i = 0; i < damagedStructuresCount; i+=1){
+    var damagedStructure = damagedStructures[i];
+    missingStructureHitpoints = missingStructureHitpoints + damagedStructure.hitsMax - damagedStructure.hits;
+    totalStructureHitpoints = totalStructureHitpoints + damagedStructure.hitsMax;
+}
+var missingStructureHitpointsPercentage = (missingStructureHitpoints / totalStructureHitpoints)*100;
+var impBld = 50 * cSitesCount + missingStructureHitpointsPercentage;
+console.log('Construction Sites | Missing Structure Hitpoints(%)= ' + cSitesCount + ' | ' + missingStructureHitpointsPercentage);
+
+// Proportions
+var impTotal = impHar + impUpg + impBld;
 var impRelHar = impHar/impTotal;
 var impRelUpg = impUpg/impTotal;
+var impRelBld = impBld/impTotal;
 var myCreeps = Game.spawns['Spawn1'].room.find(FIND_MY_CREEPS);
 var myCreepsCount = myCreeps.length;
-console.log('myCreepsCount = ' + myCreepsCount);
 var myWorkers = _.filter(Game.creeps, (creep) => creep.memory.type == 'worker');
-console.log("myWorkers = " + myWorkers);
 var myWorkersCount = myWorkers.length;
+console.log('Workers:Soldiers = ' + myWorkersCount + ':0');
 var harNr = Math.round(myWorkersCount*impRelHar);
 var upgNr = Math.round(myWorkersCount*impRelUpg);
-console.log('harNr:upgNr = ' + harNr + ':' + upgNr );
+var bldNr = Math.round(myWorkersCount*impRelBld);
+console.log('Harversters:Upgraders:Builders = ' + harNr + ':' + upgNr + ':' + bldNr);
 var changedMemory = 0;
-var workerRoles = ['harvester', 'upgrader'];
+var workerRoles = ['harvester', 'upgrader', 'builder'];
 
 module.exports = {
     run() {
@@ -44,6 +65,8 @@ module.exports = {
                 worker.memory.role = workerRoles[0];
             } else if (changedMemory < harNr + upgNr) {
                 worker.memory.role = workerRoles[1];
+            } else if (changedMemory < harNr + upgNr + bldNr) {
+                worker.memory.role = workerRoles[2];
             }
         changedMemory +=1;    
         }
--- worker-roles/main.js
+++ worker-roles/main.js
@@ -1,3 +1,4 @@
+console.log('beep2!')
 var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
